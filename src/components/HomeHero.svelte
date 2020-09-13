9e34681c4e5400aa0785ba600e22cd4315a17d27
Ratnasambhav Priyadarshi <ratnasambhav@gmail.com>
2020-09-14 00:08:56 +0530

    Started HomeHero component

---
+++ src/components/HomeHero.svelte
@@ -0,0 +1,56 @@
+<style>
+  .wrapper {
+    padding: 70px 70px;
+    background-color: rgb(25, 169, 116);
+  }
+
+  h1 {
+    font-size: 80px;
+    font-family: "Maison Neue Bold";
+  }
+
+  p {
+    font-family: "Maison Neue";
+    font-size: 27px;
+    max-width: 600px;
+  }
+
+  .social {
+    width: 220px;
+    display: flex;
+    margin-top: 40px;
+    justify-content: space-between;
+  }
+
+  .social a {
+    color: inherit;
+    font-size: 20px;
+    font-family: "Maison Neue Light";
+  }
+
+  .social a img {
+    max-height: 30px;
+  }
+</style>
+
+<div class="wrapper">
+  <h1>Ratnasambhav's Blog</h1>
+  <p>
+    Hello there!
+    <br><br>
+    I'm Ratnasambhav Priyadarshi.
+    <br><br>
+    I write about things I like, which is mostly web developemt and video games. I work at Amazon Web Services in Bengaluru.
+  </p>
+  <div class="social">
+    <a href="https://github.com/Ratnasambhav/">
+      <img src="github.png" alt="GitHub">
+    </a>
+    <a href="https://www.linkedin.com/in/ratnasambhav/">
+      <img src="linkedin.png" alt="LinkedIn">
+    </a>
+    <a href="https://twitter.com/ratnasambhav732">
+      <img src="twitter.png" alt="Twitter">
+    </a>
+  </div>
+</div>
